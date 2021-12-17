d01f511b66ba3bc7a71cd7b0497cee5beeb11e23
lihaixin <lihaixin@15099.net>
2021-12-17 11:27:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [ "$PASSWORD" ]; then
-    sed -i "s/^\(command.*x11vnc.*\)$/\1 -passwd '$PASSWORD'/" /etc/supervisord.conf
+if [ "$VNC_PASSWORD" ]; then
+    sed -i "s/^\(command.*x11vnc.*\)$/\1 -passwd '$VNC_PASSWORD'/" /etc/supervisord.conf
 fi
 
 /usr/bin/supervisord
