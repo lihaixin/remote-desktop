e68ac622d2c5dfe30ea3ce5a4257d6d4716b603d
lihaixin <lihaixin@15099.net>
2021-12-17 08:47:05 +0800

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ "$VNC_PASSWORD" ]; then
-    sed -i "s/^\(command.*x11vnc.*\)$/\1 -passwd '$VNC_PASSWORD'/" /etc/supervisord.conf
+if [ "$PASSWORD" ]; then
+    echo "alpine:$PASSWORD" | /usr/sbin/chpasswd \
+    sed -i "s/^\(command.*x11vnc.*\)$/\1 -passwd '$PASSWORD'/" /etc/supervisord.conf
 fi
 
 /usr/bin/supervisord
